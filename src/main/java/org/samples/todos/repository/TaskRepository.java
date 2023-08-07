da1396c93a536372fe9da61ad620bdf0514caf20
GeorgeQuantAnalyst <jiri.tester@outlook.com>
2023-08-07 10:19:47 +0200

    Defined TODOs for Lucka in TaskRepository

--- src/main/java/org/samples/todos/repository/TaskRepository.java
+++ src/main/java/org/samples/todos/repository/TaskRepository.java
@@ -1,4 +1,21 @@
 package org.samples.todos.repository;
 
+import org.samples.todos.model.Task;
+
+import java.util.List;
+import java.util.Map;
+
 public class TaskRepository {
+
+    public Map<String, List<Task>> load(){
+        Map<String, List<Task>> result = null;
+
+        //TODO: @Lucka please implement me
+
+        return result;
+    }
+
+    public void save(Map<String, List<Task>> tasksMap){
+        //TODO: @Lucka please implement me
+    }
 }
