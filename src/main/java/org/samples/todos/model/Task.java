5b1ba3cfaca3b718d872fd9c7731dd29dcaa2991
GeorgeQuantAnalyst <jiri.tester@outlook.com>
2023-08-07 08:59:33 +0200

    Defined TODOs for Lucka

--- src/main/java/org/samples/todos/model/Task.java
+++ src/main/java/org/samples/todos/model/Task.java
@@ -1,4 +1,14 @@
 package org.samples.todos.model;
 
 public class Task {
+
+    // TODO: @Lucka create properties
+
+    // TODO: @Lucka create constructors (empty and with parameters)
+
+    // TODO: @Lucka create getters() and setters() methods
+
+    // TODO: @Lucka create equals() and hashcode() methods
+
+    // TODO: @Lucka create toString() method
 }
