02d071fa636a6690840f0ba3c9e51e4d08aea255
GeorgeFreelanceDeveloper <jiri.tester@outlook.com>
2023-11-20 09:25:34 +0100

    Write unit tests for TaskManagerTest in Kotlin - part 2

--- kotlin/src/test/kotlin/org/samples/todos/service/TaskManagerTest.kt
+++ kotlin/src/test/kotlin/org/samples/todos/service/TaskManagerTest.kt
@@ -11,11 +11,13 @@ import org.samples.todos.repository.TaskRepository
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.test.BeforeTest
+import kotlin.test.assertFalse
 
 
 class TaskManagerTest {
 
-    private var taskManager: TaskManager? = null
+    private lateinit var taskManager: TaskManager
 
     private val taskRepository: TaskRepository
 
@@ -25,6 +27,10 @@ class TaskManagerTest {
         sdf.timeZone = TimeZone.getTimeZone("Europe/Prague")
         taskRepository = Mockito.mock(TaskRepository::class.java)
         Mockito.`when`(taskRepository.load()).thenReturn(createSampleTasks())
+    }
+
+    @BeforeTest
+    fun init() {
         taskManager = TaskManager(taskRepository)
     }
 
@@ -37,6 +43,82 @@ class TaskManagerTest {
         actualResult = taskManager!!.getAll()
 
         assertTrue(result)
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun updateTask() {
+        val task: Task = taskManager.getAll()
+            .first { it.name == "Personal" }
+            .tasks[0]
+
+        task.title = "Test update task"
+
+        val result: Boolean = taskManager.updateTask(task)
+
+        val taskAfterUpdate = taskManager.getAll().first { it.name == "Personal" }.tasks[0]
+
+        assertTrue(result)
+        assertEquals("Test update task", taskAfterUpdate.title)
+    }
+
+    @Test
+    fun deleteTask() {
+        val deleteTaskId = UUID.fromString("c5e128d1-24bf-4a4b-974d-72cbba71f9d3")
+
+        val result = taskManager.deleteTask(deleteTaskId)
+
+        val uuids = taskManager.getAll().flatMap { it.tasks }.map { it.id }
+
+        assertTrue(result)
+        assertFalse(deleteTaskId in uuids)
+    }
+
+    @Test
+    fun setDone() {
+        val taskId = UUID.fromString("f327f935-89c9-43b2-9f18-87ac967035a6");
+
+        taskManager.setDone(taskId)
+
+        val task = taskManager.getAll().flatMap { it.tasks }.first { it.id == taskId }
+
+        assertEquals(taskId, task.id)
+        assertTrue(task.done)
+    }
+
+    @Test
+    fun getAll() {
+        assertEquals(createSampleTasks(), taskManager.getAll())
+    }
+
+    @Test
+    fun getByGroupName() {
+        val expectedResult = createSampleTasks()[0].tasks
+        val actualResult = taskManager.getBy("Personal")
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun getByGroupNameAndPriority() {
+        val expectedResult = createSampleTasks()[0].tasks.filter { it.priority == Priority.LOW }
+        val actualResult = taskManager.getBy("Personal", Priority.LOW)
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun getByGroupNameAndDone() {
+        val expectedResult = createSampleTasks()[0].tasks.filter { it.done }
+        val actualResult = taskManager.getBy("Personal", true)
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun getByGroupNameAndOlderThan() {
+        val olderThanDate = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse("2023-08-03T09:00:00Z")
+
+        val expectedResult = createSampleTasks()[0].tasks.filter { it.createDate.before(olderThanDate) }
+        val actualResult = taskManager.getBy("Personal", olderThanDate)
+
         assertEquals(expectedResult, actualResult)
     }
 
@@ -60,8 +142,26 @@ class TaskManagerTest {
             sdf.parse("2023-08-02T16:15:00Z")
         )
 
+        val task3 = Task(
+            UUID.fromString("312437b1-416d-4e38-8c89-4b811ad67a6e"),
+            "Task 3",
+            "Description for Task 3",
+            Priority.LOW,
+            true,
+            sdf.parse("2023-08-01T10:00:00Z")
+        )
+
+        val task4 = Task(
+            UUID.fromString("bb90c86a-ad33-4700-bf08-cda65130025b"),
+            "Task 4",
+            "Description for Task 4",
+            Priority.HIGH,
+            false,
+            sdf.parse("2023-08-02T16:15:00Z")
+        )
+
         val personalGroup = TaskGroup("Personal", mutableListOf(task1, task2))
-        val workGroup = TaskGroup("Work", mutableListOf(task1, task2))
+        val workGroup = TaskGroup("Work", mutableListOf(task3, task4))
 
         return mutableListOf(personalGroup, workGroup)
     }
